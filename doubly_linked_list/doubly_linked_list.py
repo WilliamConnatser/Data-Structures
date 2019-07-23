d60f5f4d93518a81d341df34f2e5b85502248906
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-22 19:54:49 -0500

    finished day 1 mvp- not passing one test

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -43,26 +43,141 @@ class DoublyLinkedList:
   def __len__(self):
     return self.length
 
+  '''
+    add_to_head
+    Needs to handle two cases:
+      1- List head == None
+      2- List head != None
+  '''
   def add_to_head(self, value):
-    pass
+    if self.head == None:
+      node = ListNode(value)
+      self.head = node
+      self.tail = node
+    else:
+      self.head.insert_before(value)
+      self.head = self.head.prev
+    self.increment_len()
 
+  '''
+    remove_from_head
+    Needs to handle 3 cases:
+      1- List length == 0
+      2- List length == 1
+      3- List length > 1
+  '''
   def remove_from_head(self):
-    pass
+    removed = None
+    if self.head:
+      removed = self.head.value
+      if self.length == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head.delete()
+      self.decrement_len()
+    return removed
 
+  '''
+    add_to_tail
+    Needs to handle 2 cases:
+      1- List length == 0
+      2- List length >= 1
+  '''
   def add_to_tail(self, value):
-    pass
+    if not self.tail:
+      node = ListNode(value)
+      self.head = node
+      self.tail = node
+    else:
+      self.tail.insert_after(value)
+      self.tail = self.tail.next
+    self.increment_len()
 
+  '''
+    remove_from_tail
+    Needs to handle 2 cases:
+      1- List length == 0
+      2- List length == 1
+      3- List length > 1
+  '''
   def remove_from_tail(self):
-    pass
+    removed = None
+    if self.tail:
+      removed = self.tail.value
+      if self.length == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.tail.delete()
+      self.decrement_len()
+    return removed
 
+  '''
+    move_to_front
+      Need to cover 2 possible conditions:
+      1- Make sure the list length !== 0
+      2- Make sure the node is not already in the front
+  '''
   def move_to_front(self, node):
-    pass
+    if self.head and self.head != node:
+      node.delete()
+      self.head.insert_before(node.value)
+      self.head = self.head.prev
 
+  '''
+    move_to_end
+    Need to cover 2 possible conditions:
+      1- Make sure the list length !== 0
+      2- Make sure the node not already in the end
+      3- Change head if the node being moved to the end is the head
+  '''
   def move_to_end(self, node):
-    pass
+    if self.tail and self.tail != node:
+      node.delete()
+      self.tail.insert_after(node.value)
+      self.tail = self.tail.next
+      self.head = self.head if node != self.head else node.next
 
+  '''
+    delete
+    Need to cover 5 possible conditions:
+      1- List length == 0
+      2- List length == 1
+      3- Deleting the head node
+      4- Deleting the tail node
+      5- Deleting neither the head or the tail node (a node in the middle of the list)
+  '''
   def delete(self, node):
-    pass
-    
+    if self.head != None:
+      if self.length == 1:
+        self.head = None
+        self.tail = None
+      elif self.head == node:
+        self.head = self.head.next
+      elif self.tail == node:
+        self.tail = self.tail.prev
+      self.decrement_len()
+  
+  '''
+    get_max
+    Need to cover 2 conditions:
+      1- List length == 0
+      2- List length >= 1
+  '''
   def get_max(self):
-    pass
+    if self.head:
+      current_node = self.head
+      max_node = current_node
+      while current_node:
+        if max_node.value < current_node.value:
+          max_node == current_node
+        current_node = current_node.next
+      return max_node.value
+    return None
+
+  def increment_len(self):
+    self.length += 1
+  
+  def decrement_len(self):
+    self.length -= 1
