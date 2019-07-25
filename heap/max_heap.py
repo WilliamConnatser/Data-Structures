047a6c6b14cf9e5f3e6d9127a1f2edbc7bccf281
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-07-25 18:40:34 -0500

    finished max_heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -8,22 +8,32 @@ class Heap:
   def insert(self, value):
     #Append value to the array
     self.storage.append(value)
-    #Bubble up the tree
-    index = len(self.storage)-1
-    self._bubble_up(index)
+    #Bubble the value to the proper position in the tree
+    self._bubble_up(len(self.storage)-1)
 
   '''`delete` removes and returns the 'topmost' value from the heap;
       this method needs to ensure that the heap property is
       maintained after the topmost element has been removed.'''
   def delete(self):
-    topmost = self.storage[0]
-    self.storage = self.storage[1:]
-    self._sift_down(0)
+    if len(self.storage) > 2:
+      self._swap(0,len(self.storage)-1)
+      topmost = self.storage.pop()
+      self._sift_down(0)
+    elif len(self.storage) == 2:
+      self._swap(0,len(self.storage)-1)
+      topmost = self.storage.pop()
+      self._sift_down(0) #unnecessary, but needed to pass the tests..
+    elif len(self.storage) == 1:
+      topmost = self.storage.pop()
+    else:
+      topmost = None
     return topmost
 
   '''`get_max` returns the maximum value in the heap _in constant time_.'''
   def get_max(self):
-    return self.storage[0]
+    if self.storage[0]:
+      return self.storage[0]
+    return None
 
   '''`get_size` returns the number of elements stored in the heap.'''
   def get_size(self):
@@ -33,43 +43,61 @@ class Heap:
       by swapping it with its parent if the parent's value is less than
       the value at the specified index.'''
   def _bubble_up(self, index):
-    if self._parent(index) and self._parent(index) < self.storage[index]:
-      parent_index = index-1//2  
-      self.storage[index, parent_index] = self.storage[parent_index,index]
-      self._bubble_up(parent_index)
-      self._sift_down(index) #Not sure if needed?
+    parent = self._parent(index)
+    
+    if parent and parent["value"] < self.storage[index]:
+      self._swap(parent["index"],index)      
+      self._bubble_up(parent["index"])      
 
   '''`_sift_down` grabs the indices of this element's children and determines
     which child has a larger value. If the larger child's value is larger
     than the parent's value, the child element is swapped with the parent.'''
   def _sift_down(self, index):
-    if self._left_child(index) and self._left_child(index) > self.storage[index]:
-      left_child_index = 2*index+1
-      self.storage[index],self.storage[left_child_index] = self.storage[left_child_index],self.storage[index]
-      self._sift_down(left_child_index)
-      self._bubble_up(index) #Not sure if needed?
-    elif self._right_child(index) and self._right_child(index) > self.storage[index]:
-      right_child_index = 2*index+2
-      self.storage[index],self.storage[right_child_index] = self.storage[right_child_index],self.storage[index]
-      self._sift_down(right_child_index)
-      self._bubble_up(index) #Not sure if needed?
+    left_child = self._left_child(index)
+    right_child = self._right_child(index)
+    largest_index = index
+    print(f"^^Sifting Down^^ \nCurrent Value={self.storage[index]} \nLeft Child={left_child} \nRight Child={right_child} \nHeap Before={self.storage}")
+    if left_child and self.storage[largest_index] < left_child["value"]:
+      largest_index = left_child["index"]
+    if right_child and self.storage[largest_index] < right_child["value"]:
+      largest_index = right_child["index"]
+    if largest_index != index:
+      self._swap(index,largest_index)
+      print(f"Heap After= {self.storage} \n\n")
+      self._sift_down(largest_index)
+    else:
+      print("\n\n")
 
-  def _parent(self, index):
-    index = index-1//2
-    if index >= 0 and index <= len(self.storage)-1:
-      return self.storage[index]
-    return None
+  def _parent(self,index):
+    computed_index = index-1
+    computed_index = computed_index//2
+    if computed_index < len(self.storage) and computed_index >= 0:
+      return {
+        "index": computed_index,
+        "value": self.storage[computed_index]
+      }
+    else:
+      return None
+  
+  def _left_child(self,index):
+    computed_index = 2*index+1
+    if computed_index < len(self.storage) and computed_index >= 0:
+      return {
+        "index": computed_index,
+        "value": self.storage[computed_index]
+      }
+    else:
+      return None
 
-  #Returns left child
-  def _left_child(self, index):
-    index = 2*index+1
-    if index >= 0 and index <= len(self.storage)-1:
-      return self.storage[index]
-    return None
+  def _right_child(self,index):
+    computed_index = 2*index+2
+    if computed_index < len(self.storage) and computed_index >= 0:
+      return {
+        "index": computed_index,
+        "value": self.storage[computed_index]
+      }
+    else:
+      return None
 
-  #Returns right child
-  def _right_child(self, index):
-    index = 2*index+2
-    if index >= 0 and index <= len(self.storage)-1:
-      return self.storage[index]
-    return None
+  def _swap(self,x,y):
+    self.storage[x],self.storage[y] = self.storage[y],self.storage[x]
